c5bb7d2c2ded246ee09ed0e5d6700424840c224c
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-06-28 19:59:32 +0600

    Query In RTK Query

--- To-Do/src/components/Todo/TodoContainer.tsx
+++ To-Do/src/components/Todo/TodoContainer.tsx
@@ -1,10 +1,15 @@
-import { useAppSelector } from "../../redux/features/hooks";
+import { useGetTodosQuery } from "../../redux/api/api";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <div className="flex justify-between mb-5">
@@ -13,10 +18,8 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full rounded-lg space-y-3">
-          {todos.map((item) => (
-            <TodoCard
-             {...item}
-            ></TodoCard>
+          {todos?.data.map((item) => (
+            <TodoCard {...item}></TodoCard>
           ))}
         </div>
         {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center rounded-md">
--- To-Do/src/redux/api/api.ts
+++ To-Do/src/redux/api/api.ts
@@ -1,14 +1,16 @@
-
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:5000' }),
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
   endpoints: (builder) => ({
-    getPokemonByName: builder.query<Pokemon, string>({
-      query: (name) => `pokemon/${name}`,
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks",
+        method: "GET",
+      }),
     }),
   }),
-})
+});
 
-export const { useGetPokemonByNameQuery } = baseApi
+export const { useGetTodosQuery } = baseApi;
